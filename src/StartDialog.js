e674f9d1b582857137fc907cfb5f6cda8e336137
jacobwmorris <jacobwmorris0@gmail.com>
2023-06-22 16:34:57 -0400

    Add StartDialog

---
+++ src/StartDialog.js
@@ -0,0 +1,36 @@
+
+function StartDialog({characters, handleStart}) {
+  const charactersRendered = Object.keys(characters).map((name) => {
+    return <Portrait key={name} name={name}/>;
+  });
+
+  return (
+    <div>
+      <div>
+        <h1>Where's That Meme?</h1>
+        <p>
+          Look for the following internet legends in the picture.
+          When you find them, click on them and then select their name.
+        </p>
+        <strong>You're looking for:</strong>
+        <div>
+          {charactersRendered}
+        </div>
+        <button onClick={(e) => handleStart()}></button>
+      </div>
+    </div>
+  )
+}
+
+function Portrait({name}) {
+  //TODO: Get the character's portait from firebase
+
+  return (
+    <li>
+      <div>{name}</div>
+      <img src="#" alt={name}/>
+    </li>
+  )
+}
+
+export default StartDialog;
---
+++ src/tests/StartDialog.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import {render, screen} from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import StartDialog from "../StartDialog";
+
+const sampleChars = {
+  "Dramatic Chipmunk": {x: 0, y: 0, found: true},
+  "Chuck Testa": {x: 1, y: 0, found: false},
+  "Mudkip": {x: 0, y: 1, found: false}
+}
+
+it("Renders character list", () => {
+  render(<StartDialog characters={sampleChars} handleStart={() => {}}/>);
+  expect(screen.getByText("Dramatic Chipmunk")).toBeInTheDocument();
+  expect(screen.getByText("Chuck Testa")).toBeInTheDocument();
+  expect(screen.getByText("Mudkip")).toBeInTheDocument();
+})
